6f6c4084005a3c881b04808cc803d6153c97aaa1
Ray <rainer.musch@gmx.de>
2025-01-10 12:19:55 +0100

    add tasks to script.js

---
+++ script/script.js
@@ -0,0 +1,28 @@
+// Aufgaben
+// *** Hauptbildschirm / kleine Ansicht ***
+// Es soll eine bestimmte Anzahl an Pokemon Karten direkt gerendert werden. Am besten zwischen 20 und 40
+// Unten gibt es einen Button, um weitere 20-40 Pokemon zu laden. (Info: insgesamt gibt es über 1000 Pokemon)
+// Beim laden erscheint ein Loadingscreen (Userfeedback)
+// Der Button zum Nachladen kann während des Ladens nicht erneut angeklickt werden
+// Sichtbar soll auf jeder kleinen Pokemon Karte sein: Name, Typ/en, Bild des Pokemons, Hintergrundfarbe passend zum Typ, ID (optional)
+// Es soll einen Hover-Effekt auf der kleinen Pokemon Karte geben: cursor-pointer,  Pokemon erscheint größer etc. (optional)
+// *** Große Ansicht: *** 
+// Beim Klicken auf die Pokemonkarte soll sich diese in groß öffnen.
+// Benutze ein transparentes Overlay, beim Klicken darauf schließt sich die Karte wieder (wie beim Dialog Fenster). Der Hintergrund ist nicht scrollbar in der großen Ansicht.
+// Wie du diese gestaltet und welche du hier alle anzeigen lässt, ist dir überlassen, jedoch sollten hier mindestens gewisse Werte wie z.B. hp/ attack/ defense etc. des Pokemon angezeigt werden, weiteres ist Optional.
+// Es gibt Pfeile oder ähnliches, um zwischen den Karten in der großen Ansicht zu wechseln (wie bei der Fotogalerie).
+// *** Code ***
+// Aussagekräftige Namen für Funktionen und Variablen
+// camelCase für die Benennung 
+// Code ist formatiert
+// Höchstens 14 Zeilen pro Funktion
+// Gleicher Abstand zwischen Funktionen (1 oder 2 Leerzeilen)
+// Lagere HTML Templates aus in extra-Funktionen
+// *** Responsive ***
+// Bis 320px Breite alles responsive ohne Scrollbalken
+// Content-Begrenzung für große Monitore (max-width z.B. bei 1920px oder 1440px)
+// *** Sonstiges ***
+// Favicon
+// Dokumenten Titel
+// Header mit: Logo, Titel, Suchleiste (man soll mindestens 3 Buchstaben eingeben bevor gesucht werden kann, wenn diese Buchstaben Teil des Namens eines Pokemons sind, sollten diese Pokemon angezeigt werden. Es sollte eine begrenzte Anzahl an Pokemon mit den Suchkriterien angezeigt werden, z.B. 10 stück)
+// Footer (optional)
